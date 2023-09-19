9f072a53c6e5a841f6e29d61aea913ecc69efd30
Salatik-N <nikita-salata@mail.ru>
2023-09-19 15:27:04 +0200

    filter

--- components/CargoFilter.tsx
+++ components/CargoFilter.tsx
@@ -8,8 +8,10 @@ import address from '../lib/city.json'
 
 enum FIELDS {
   SHIPPING_REGION = 'shippingRegion',
+  SHIPPING_CITY = 'shippingCity',
   UNLOADING_COUNTRY = 'unloadingCountry',
   UNLOADING_REGION = 'unloadingRegion',
+  UNLOADING_CITY = 'unloadingCity',
   WEIGHT = 'weight',
   VEHICLE_BODY_TYPE = 'vehicleBodyType',
   ORDER_BY = 'orederBy',
@@ -18,8 +20,10 @@ enum FIELDS {
 export default function CargoFilter({ onUseFilter }) {
   const [form, setForm] = useState({
     [FIELDS.SHIPPING_REGION]: null,
+    [FIELDS.SHIPPING_CITY]: null,
     [FIELDS.UNLOADING_COUNTRY]: null,
     [FIELDS.UNLOADING_REGION]: null,
+    [FIELDS.UNLOADING_CITY]: null,
     [FIELDS.WEIGHT]: null,
     [FIELDS.VEHICLE_BODY_TYPE]: null,
     [FIELDS.ORDER_BY]: null,
@@ -43,7 +47,7 @@ export default function CargoFilter({ onUseFilter }) {
   const NoOptionsMessage = (props) => {
     return (
       <components.NoOptionsMessage {...props}>
-        <span className="custom-css-class">Выберите страну</span>
+        <span>Выберите предыдущий параметр</span>
       </components.NoOptionsMessage>
     )
   }
@@ -73,6 +77,21 @@ export default function CargoFilter({ onUseFilter }) {
             />
           </label>
           <label>
+            <span>Город</span>
+            <Select
+              name={FIELDS.SHIPPING_CITY}
+              onChange={handleChangeFormSelect}
+              options={
+                address.find((e) => e.name === 'Беларусь').regions.find((e) => e.name === form[FIELDS.SHIPPING_REGION])
+                  ?.cities
+              }
+              components={{ NoOptionsMessage }}
+              getOptionLabel={(option) => option.name}
+              getOptionValue={(option) => option.name}
+              placeholder="Выберите город"
+            />
+          </label>
+          <label>
             <span>Страна прибытия</span>
             <Select
               name={FIELDS.UNLOADING_COUNTRY}
@@ -93,6 +112,21 @@ export default function CargoFilter({ onUseFilter }) {
               getOptionValue={(option) => option.name}
               components={{ NoOptionsMessage }}
               placeholder="Все"
+            />
+          </label>
+          <label>
+            <span>Город прибытия</span>
+            <Select
+              name={FIELDS.UNLOADING_CITY}
+              onChange={handleChangeFormSelect}
+              options={
+                address
+                  .find((e) => e.name === form[FIELDS.UNLOADING_COUNTRY])
+                  ?.regions.find((e) => e.name === form[FIELDS.UNLOADING_REGION])?.cities
+              }
+              getOptionLabel={(option) => option.name}
+              getOptionValue={(option) => option.name}
+              placeholder="Выберите город"
             />
           </label>
           <label>
